1f839bb6947e9f4e7e6ed90610bcc8e4c9ac9c84
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2025-10-06 15:28:42 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import cors from "cors";
+import express, { Request, Response } from "express";
+import path from "path";
+import { StatusCodes } from "http-status-codes";
+// import globalErrorHandler from './app/middlewares/globalErrorHandler';
+// import router from './routes';
+const app = express();
+
+//body parser
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//file retrieve
+app.use(express.static("uploads"));
+app.use(express.urlencoded({ extended: true })); // To handle URL-encoded data
+
+// Serve static files for uploaded images
+app.use("/public", express.static(path.join(__dirname, "../public")));
+
+//router
+// app.use('/api/v1', router);
+
+//live response
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+//global error handle
+// app.use(globalErrorHandler);
+
+//handle not found route;
+app.use((req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: "Not found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API DOESN'T EXIST",
+      },
+    ],
+  });
+});
+
+export default app;
